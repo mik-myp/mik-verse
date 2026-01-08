7a5b8367461ec6174a157f0cf603e452372c1b44
mayupeng <2836365261@qq.com>
2026-01-08 15:57:22 +0800

    feat：测试CICD

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <div>测试CICD</div>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
